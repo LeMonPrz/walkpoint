1a4a66f7d9d489406421838865efa62bec62ef67
Api Phoom <apiphoom23@gmail.com>
2024-03-12 21:37:53 +0700

    feat : fix tabbar logic

--- Login Form/ContentView.swift
+++ Login Form/ContentView.swift
@@ -24,83 +24,84 @@ struct MainView: View {
     
     var body: some View {
         TabView{
-            HistoryView()
-            .tabItem(){
-                Label("Menu", systemImage: "list.dash")
-            }
-            MainView()
-                .tabItem{
-                    Label("Menu2", systemImage: "list.dash")
+            LoginView()
+                .tabItem {
+                    Label("Login", systemImage: "person.fill")
                 }
-                    ZStack {
-                        Color.brown.edgesIgnoringSafeArea(.all)
+            
+            HistoryView()
+                .tabItem {
+                    Label("History", systemImage: "clock.fill")
+                }
+            ZStack {
+                Color.brown.edgesIgnoringSafeArea(.all)
+                VStack{
+                    Text("USER LOGIN")
+                        .bold()
+                        .font(.system(size: 44))
+                    ZStack{
+                        RoundedRectangle(cornerRadius: 15)
+                            .fill(Color.white)
+                            .frame(width: 330, height: 450)
                         VStack{
-                            Text("USER LOGIN")
-                                .bold()
-                                .font(.system(size: 44))
-                            ZStack{
-                                RoundedRectangle(cornerRadius: 15)
-                                    .fill(Color.white)
-                                    .frame(width: 330, height: 450)
-                                VStack{
-                                    VStack(alignment: .leading){
-                                        Text("Username:") // Label
-                                            .bold()
-                                            .offset(x:10)
-                                        TextField("Enter username", text: $username)
-                                            .frame(width: 250)// Input box
-                                            .foregroundColor(.brown)
-                                            .textFieldStyle(RoundedBorderTextFieldStyle())
-                                            .padding(.horizontal)
-                                    }
-                                    VStack(alignment: .leading){
-                                        Text("User ID:")
-                                            .bold()
-                                            .offset(x:10)
-                                        TextField("Enter User ID", text: $userid)
-                                            .frame(width: 250)// Input box
-                                            .foregroundColor(.brown)
-                                            .textFieldStyle(RoundedBorderTextFieldStyle())
-                                            .padding(.horizontal)
-                                    }
-                                    VStack(alignment: .leading){
-                                        Text("Password:") // Label
-                                            .bold()
-                                            .offset(x:10)
-                                        TextField("Enter Password", text: $password)
-                                            .frame(width: 250)// Input box
-                                            .foregroundColor(.brown)
-                                            .textFieldStyle(RoundedBorderTextFieldStyle())
-                                            .padding(.horizontal)
-                                    }
-                                    Button(action: login) {
-                                        Text("Login")
-                                            .foregroundColor(.white)
-                                            .padding()
-                                            .frame(width: 200, height: 50)
-                                            .background(
-                                                RoundedRectangle(cornerRadius: 10)
-                                                    .fill(Color.brown)
-                                            )
-                                    }
-                                    Button(action: signIn) {
-                                        Text("Sign up")
-                                            .foregroundColor(.blue)
-                                            .padding()
-                                    }
-                                }
+                            VStack(alignment: .leading){
+                                Text("Username:") // Label
+                                    .bold()
+                                    .offset(x:10)
+                                TextField("Enter username", text: $username)
+                                    .frame(width: 250)// Input box
+                                    .foregroundColor(.brown)
+                                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                                    .padding(.horizontal)
+                            }
+                            VStack(alignment: .leading){
+                                Text("User ID:")
+                                    .bold()
+                                    .offset(x:10)
+                                TextField("Enter User ID", text: $userid)
+                                    .frame(width: 250)// Input box
+                                    .foregroundColor(.brown)
+                                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                                    .padding(.horizontal)
+                            }
+                            VStack(alignment: .leading){
+                                Text("Password:") // Label
+                                    .bold()
+                                    .offset(x:10)
+                                TextField("Enter Password", text: $password)
+                                    .frame(width: 250)// Input box
+                                    .foregroundColor(.brown)
+                                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                                    .padding(.horizontal)
+                            }
+                            Button(action: login) {
+                                Text("Login")
+                                    .foregroundColor(.white)
+                                    .padding()
+                                    .frame(width: 200, height: 50)
+                                    .background(
+                                        RoundedRectangle(cornerRadius: 10)
+                                            .fill(Color.brown)
+                                    )
+                            }
+                            Button(action: signIn) {
+                                Text("Sign up")
+                                    .foregroundColor(.blue)
+                                    .padding()
                             }
                         }
                     }
                 }
+            }
         }
-        func login() {
-            print("Invalid username or password")
-        }
-        
-        func signIn() {
-            print("Sign in button tapped")
-        }
+    }
+    func login() {
+        print("Invalid username or password")
+    }
+    
+    func signIn() {
+        print("Sign in button tapped")
+    }
 }
 
 struct MainView_Previews: PreviewProvider {
